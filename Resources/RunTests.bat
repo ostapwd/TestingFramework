723d9b03b010d07b14cac5539a34df4055accbd6
ostapwdwd <ostapwdwd@gmail.com>
2019-03-09 15:24:55 +0200

    updated script

--- Resources/RunTests.bat
+++ Resources/RunTests.bat
@@ -1,5 +1,5 @@
 SET RootPath=%cd%
-SET DllPath=%RootPath%\..\TestingFramework\bin\Debug\TestingFramework.dll
+SET DllPath="%RootPath%\..\TestingFramework\bin\Debug\TestingFramework.dll"
 
 CD "%RootPath%\NUnit.Console-3.9.0"
 nunit3-console %DllPath%
