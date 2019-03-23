cc0145f205089edf88f05d83e63836013b66df57
modest-evil <35898373+modest-evil@users.noreply.github.com>
2019-03-23 13:31:56 +0200

    feedback test added

--- TestingFramework/Pages/MesgEbayPage.cs
+++ TestingFramework/Pages/MesgEbayPage.cs
@@ -1,14 +1,34 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestingFramework.CustomWebElements;
+using TestingFramework.Tools;
 
 namespace TestingFramework.Pages
 {
     public class MesgEbayPage : AbstractBasePageWithSection
     {
+        [FindsBy(How = How.CssSelector, Using = "#feedback-lnk")]
+        private readonly IWebElement _FeedbackLinkWebElement;
 
+        public Link FeedbackLinkWebElement
+        {
+            get
+            {
+                return new Link(_FeedbackLinkWebElement);
+            }
+            private set { }
+        }
 
+        public FeedbackFormPage ReachFeedbackForm()
+        {
+            FeedbackLinkWebElement.Click();
+            Driver.SwitchToNewTab();
+            return new FeedbackFormPage();
+        }
     }
 }
---
+++ TestingFramework/Tests/FeedbackTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TestingFramework.Pages;
+using TestingFramework.TestData;
+
+namespace TestingFramework.Tests
+{
+    [TestFixture]
+    class FeedbackTest : BaseTest
+    {
+        private LoginPage _loginPage;
+        private HomePage _homepage;
+        private MyEbayPage _myEbayPage;
+        private MesgEbayPage _mesgEbayPage;
+        private FeedbackFormPage _feedbackFormPage;
+
+        [OneTimeSetUp]
+        public void OpenLoginPage()
+        {
+            StartPage.OpenLoginPage();
+            _loginPage = new LoginPage();
+        }
+
+        [Test]
+        public void ReachFeedbackPageTest()
+        {
+            _homepage = _loginPage.Login(TestData.UserData.Login, TestData.UserData.Password);
+            _myEbayPage = _homepage.OpenMyEbayPage();
+            _mesgEbayPage = _myEbayPage.OpenMessagesPage();
+            _feedbackFormPage = _mesgEbayPage.ReachFeedbackForm();
+        }
+
+        [OneTimeTearDown]
+        public void WaitBeforeClosing()
+        {
+            Thread.Sleep(10000);
+        }
+    }
+}
